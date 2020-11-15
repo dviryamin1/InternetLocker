ba47766f28e8b7bdf43936e266e86d45892573c7
dviryamin1 <dviryamin1@users.noreply.github.com>
2020-11-15 18:52:27 +0200

    Add project files.

---
+++ IntenetLocker/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace IntenetLocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enableBtn = new System.Windows.Forms.Button();
+            this.disableBtn = new System.Windows.Forms.Button();
+            this.enAllBtn = new System.Windows.Forms.Button();
+            this.disAllBtn = new System.Windows.Forms.Button();
+            this.interfacesList = new System.Windows.Forms.CheckedListBox();
+            this.cmdWin = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // enableBtn
+            // 
+            this.enableBtn.Location = new System.Drawing.Point(12, 219);
+            this.enableBtn.Name = "enableBtn";
+            this.enableBtn.Size = new System.Drawing.Size(187, 42);
+            this.enableBtn.TabIndex = 0;
+            this.enableBtn.Text = "Enable Internet connection";
+            this.enableBtn.UseVisualStyleBackColor = true;
+            this.enableBtn.Click += new System.EventHandler(this.enableBtn_Click);
+            // 
+            // disableBtn
+            // 
+            this.disableBtn.Location = new System.Drawing.Point(366, 219);
+            this.disableBtn.Name = "disableBtn";
+            this.disableBtn.Size = new System.Drawing.Size(187, 42);
+            this.disableBtn.TabIndex = 1;
+            this.disableBtn.Text = "Disable Internet connection";
+            this.disableBtn.UseVisualStyleBackColor = true;
+            this.disableBtn.Click += new System.EventHandler(this.disableBtn_Click);
+            // 
+            // enAllBtn
+            // 
+            this.enAllBtn.Location = new System.Drawing.Point(12, 157);
+            this.enAllBtn.Name = "enAllBtn";
+            this.enAllBtn.Size = new System.Drawing.Size(187, 42);
+            this.enAllBtn.TabIndex = 2;
+            this.enAllBtn.Text = "Enable All Internet connection";
+            this.enAllBtn.UseVisualStyleBackColor = true;
+            this.enAllBtn.Click += new System.EventHandler(this.enAllBtn_Click);
+            // 
+            // disAllBtn
+            // 
+            this.disAllBtn.Location = new System.Drawing.Point(366, 157);
+            this.disAllBtn.Name = "disAllBtn";
+            this.disAllBtn.Size = new System.Drawing.Size(187, 42);
+            this.disAllBtn.TabIndex = 3;
+            this.disAllBtn.Text = "Disable All Internet connection";
+            this.disAllBtn.UseVisualStyleBackColor = true;
+            this.disAllBtn.Click += new System.EventHandler(this.disAllBtn_Click);
+            // 
+            // interfacesList
+            // 
+            this.interfacesList.FormattingEnabled = true;
+            this.interfacesList.HorizontalScrollbar = true;
+            this.interfacesList.Location = new System.Drawing.Point(12, 12);
+            this.interfacesList.Name = "interfacesList";
+            this.interfacesList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.interfacesList.Size = new System.Drawing.Size(224, 109);
+            this.interfacesList.TabIndex = 4;
+            // 
+            // cmdWin
+            // 
+            this.cmdWin.AutoSize = true;
+            this.cmdWin.Enabled = false;
+            this.cmdWin.Location = new System.Drawing.Point(299, 23);
+            this.cmdWin.Name = "cmdWin";
+            this.cmdWin.Size = new System.Drawing.Size(119, 17);
+            this.cmdWin.TabIndex = 5;
+            this.cmdWin.Text = "Show CMD window";
+            this.cmdWin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 273);
+            this.Controls.Add(this.cmdWin);
+            this.Controls.Add(this.interfacesList);
+            this.Controls.Add(this.disAllBtn);
+            this.Controls.Add(this.enAllBtn);
+            this.Controls.Add(this.disableBtn);
+            this.Controls.Add(this.enableBtn);
+            this.Name = "Form1";
+            this.Text = "Internet Locker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enableBtn;
+        private System.Windows.Forms.Button disableBtn;
+        private System.Windows.Forms.Button enAllBtn;
+        private System.Windows.Forms.Button disAllBtn;
+        private System.Windows.Forms.CheckedListBox interfacesList;
+        private System.Windows.Forms.CheckBox cmdWin;
+    }
+}
+
---
+++ IntenetLocker/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Management.Infrastructure;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+namespace IntenetLocker
+{
+    public partial class Form1 : Form
+    {
+        //private const string InterfaceName = "wifi";
+        //private static string strCmdText = "netsh interface set interface \"" + InterfaceName + "\" ";
+        private static NetworkInterface[] list = NetworkInterface.GetAllNetworkInterfaces();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            for (int i = 0; i < list.Length; i++)
+            {
+                interfacesList.Items.Add(list[i].Name);
+            }
+            
+
+        }
+
+        private void enableBtn_Click(object sender, EventArgs e)
+        {
+            
+            foreach(string adap in interfacesList.CheckedItems)
+            {
+                Enable(adap);
+            }
+            
+        }
+
+        private void disableBtn_Click(object sender, EventArgs e)
+        {
+            foreach (string adap in interfacesList.CheckedItems)
+            {
+                Disable(adap);
+            }
+        }
+
+        static void Enable(string interfaceName)
+        {
+            ProcessStartInfo psi =
+                   new ProcessStartInfo("CMD.exe", "/C netsh interface set interface \""+interfaceName+"\" Enable");
+            psi.Verb = "runas";
+            psi.FileName = "cmd.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+
+        }
+
+        static void enableAll()
+        {
+            string com = "";
+            foreach (NetworkInterface adap in list)
+            {
+                com += "netsh interface set interface \"" + adap.Name + "\" Enable&";
+            }
+            ProcessStartInfo psi =
+                   new ProcessStartInfo("CMD.exe", "/K" + com);
+            psi.Verb = "runas";
+            psi.FileName = "cmd.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+            //p.WaitForExit();
+
+        }
+
+        static void disableAll()
+        {
+            string com = "";
+            foreach (NetworkInterface adap in list)
+            {
+                com += "netsh interface set interface \"" + adap.Name + "\" Disable&";
+            }
+            ProcessStartInfo psi =
+                   new ProcessStartInfo("CMD.exe", "/K " + com);
+            psi.Verb = "runas";
+            psi.FileName = "cmd.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+            //p.WaitForExit();
+        }
+
+        static void Disable(string interfaceName)
+        {
+
+            ProcessStartInfo psi =
+                   new ProcessStartInfo("CMD.exe", "/C  netsh interface set interface \"" + interfaceName + "\" Disable");
+            psi.Verb = "runas";
+            psi.FileName = "cmd.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //startInfo.FileName = "cmd.exe";
+            //startInfo.Arguments = strCmdText + " Disable";
+            //startInfo.Verb = "runas";
+            //process.StartInfo = startInfo;
+            //process.Start();
+        }
+
+        static void QueryInstanceFunc()
+        {
+            CimSession session = CimSession.Create("localHost");
+            IEnumerable<CimInstance> queryInstance = session.QueryInstances(@"root\cimv2", "WQL", "SELECT * FROM Win32_NetworkAdapter");
+
+            foreach (CimInstance cimObj in queryInstance)
+            {
+                Console.WriteLine(cimObj.CimInstanceProperties["Name"].ToString());
+                Console.WriteLine(cimObj.CimInstanceProperties["Description"].ToString());
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private void enAllBtn_Click(object sender, EventArgs e)
+        {
+            enableAll();
+        }
+
+        private void disAllBtn_Click(object sender, EventArgs e)
+        {
+            disableAll();
+        }
+    }
+}
